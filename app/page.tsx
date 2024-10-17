189f46126a576c087283f999996adc47378fc076
Rua <rua.elkasheef@gmail.com>
2024-10-17 05:12:13 -0400

    adding admin modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-// import { PatientForm } from "@/components/forms/PatientForm";
 import Link from "next/link";
 import Image from "next/image";
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 
-export default function Home() {
+export default function Home({ searchParams}: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
+  
   return (
     <div className="flex h-screen max-h-screen" >
 
-{/* TODO: OTP VERIFICATION | PasskeyModal */}
+    {isAdmin && <PasskeyModal/>}
 
         <section className="remover-scrollbar container my-auto">
           <div className="sub-container max-w-[496px]">
