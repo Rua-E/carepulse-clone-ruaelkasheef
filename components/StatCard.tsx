6461e13e3920157e5f1d8c6487a7342c65de8c47
Rua <rua.elkasheef@gmail.com>
2024-10-17 06:14:44 -0400

    completed admin authorization page, working on admin welcome page

---
+++ components/StatCard.tsx
@@ -0,0 +1,19 @@
+import clsx from 'clsx'
+import React from 'react'
+
+interface StatCardProps {
+  type:  'appointments' | 'pending' | 'cancelled'
+  count: number 
+  label: string
+  icon: string
+}
+
+const StatCard = ({ count = 0, label, icon, type }: StatCardProps) => {
+  return (
+    <div className={clsx('stat-card')}>
+      
+    </div>
+  )
+}
+
+export default StatCard
